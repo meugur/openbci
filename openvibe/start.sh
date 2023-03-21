9c727b182edffd1132f0d1c78300cf94de8a7ebc
Muhammed Ugur <meugur@umich.edu>
2023-03-21 15:51:04 -0400

    Added signal data

--- openvibe/start.sh
+++ openvibe/start.sh
@@ -2,11 +2,14 @@
 
 set +e
 
+mkdir -p signals
+
 docker run -it --rm \
     --privileged \
     --pid=host \
     --device=/dev/ttyUSB0 \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $PWD/signals:/signals \
     --name openvibe-container \
     openvibe
